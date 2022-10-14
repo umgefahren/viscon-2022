d68ab541d2b58b6d33e784996aac81e988bf7f40
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-10-14 11:55:49 +0200

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card w-96 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">Welcome!</h2>
+    <p>We will win this hackathon!</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
