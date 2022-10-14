e6d5709060fcc5d1784be3aa9102078ed57cce44
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-10-14 12:34:47 +0200

    Improved README

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="card w-96 bg-base-100 shadow-xl">
-  <div class="card-body">
-    <h2 class="card-title">Welcome!</h2>
-    <p>We will win this hackathon!</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+    <div class="card-body">
+      <h2 class="card-title">
+        Welcome!
+      </h2>
+      <p>We will win this hackathon!</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">
+          Buy Now
+        </button>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script lang="ts">
